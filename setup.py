55aaf472e0034055ece150a6bd64536af032907a
Jason Viloria <jnvilo@gmail.com>
2021-06-20 20:48:56 +0300

    bumped to 0.0.15

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name=PACKAGE,
-    version="0.0.15",
+    version="0.0.16",
     packages=[PACKAGE],
     long_description = long_description,
     long_description_content_type="text/markdown",
