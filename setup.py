26f812b5a4b34a20bd703704ca58a0b97451cbac
Jason Viloria <jnvilo@gmail.com>
2021-06-22 22:28:52 +0300

    updated to 0.0.17

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name=PACKAGE,
-    version="0.0.16",
+    version="0.0.17",
     packages=[PACKAGE],
     long_description = long_description,
     long_description_content_type="text/markdown",
